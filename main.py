f31f75ce75926321f9b6c67f48e40ee92a078729
miguelondigital <miguexcap@gmail.com>
2026-02-25 19:44:21 -0800

    Some board functionality and heuristic 1

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import random
 import sys
 
+#define goal state
+GOAL = [[0,1,2][3,4,5],[6,7,8]]
+
 #define node for A* search
 class Node:
     def __init__(self, puzzle, board, heuristic, parent = None, action= None):
@@ -19,25 +22,24 @@ class Node:
     #determine h according to appropriate heuristic
     def h(self):
         if(self.heuristic == 1):
-            return self.puzzle.misplaced
+            h_val = num_misplaced(self.board)
         else:
-            return self.puzzle.manhattan
+            h_val = manhattan_distance(self.board)
+        return h_val
+            
         
     #calculate f(n) = g(n) + h(n)
     def f(self):
         return self.g + self.h
 
-
-
-
-
-# class board
-
-
-
 #function to calculate heuristic 1
-def num_misplaced():
-    print('placeholder')
+def num_misplaced(board):
+    total = 0
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] != GOAL[i][j]:
+                total += 1
+    return total
 
 #function to calculate heuristic 2
 def manhattan_distance():
@@ -61,7 +63,79 @@ def manual_input():
             col += 1
         row += 1
     return new_board
+
+#function to print board
+def display_board(board):
+    for i in range (3):
+        printable = ""
+        for j in range(3):
+            printable += str(board[i][j])
+            printable += " "
+        print(printable)
+
+#function to locate the index of the empty slot in puzzle
+def find_blank(board):
+    index = (0,0)
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == 0:
+                return (i, j)
+            else:
+                continue
+
+def find_moves(blank_index):
+    # blank_index = find_blank(board)
+    valid_moves = []
+    #check up, down, left, right as valid moves
+    if (blank_index[0] - 1 >= 0):
+        valid_moves.append("up")
+    if(blank_index[0] + 1 <= 2):
+        valid_moves.append("down")
+
+    if(blank_index[1] - 1 >= 0):
+        valid_moves.append("left")
+    if(blank_index[1] + 1 <= 2):
+        valid_moves.append("right")
+
+    return(valid_moves)
+
+#function to generate all possible children given a set of valid moves
+def spawn_children(board, moves, empty_index):
+    children = []
+    for item in moves:
+        new_board = [[0]*3 for i in range (3)]
+        for i in range(3):
+            for j in range(3):
+                new_board[i][j] = board[i][j]
+        move = ""
+        if item == "left":
+            move = "left"
+            swap_placeholder = board[empty_index[0]][empty_index[1]-1]
+            new_board[empty_index[0]][empty_index[1]] = swap_placeholder
+            new_board[empty_index[0]][empty_index[1]-1] = 0
         
+        if item == "right":
+            move = "right"
+            swap_placeholder = board[empty_index[0]][empty_index[1]+1]
+            new_board[empty_index[0]][empty_index[1]] = swap_placeholder
+            new_board[empty_index[0]][empty_index[1]+1] = 0
+
+        if item == "up":
+            move = "up"
+            swap_placeholder = board[empty_index[0]-1][empty_index[1]]
+            new_board[empty_index[0]][empty_index[1]] = swap_placeholder
+            new_board[empty_index[0]-1][empty_index[1]] = 0
+        
+        if item == "down":
+            move = "down"
+            swap_placeholder = board[empty_index[0]+1][empty_index[1]]
+            new_board[empty_index[0]][empty_index[1]] = swap_placeholder
+            new_board[empty_index[0]+1][empty_index[1]] = 0
+        move_pair = (move, new_board)
+        children.append(move_pair)
+    
+    return children
+
 
 def main():
     # main entry point of the script
@@ -78,12 +152,22 @@ def main():
         unsolved = [[0]*3 for i in range (3)]
         if selection == 1:
             print("randomizing")
+
         elif selection == 2:
             print("Input Board: ")
-            unsolved = manual_input()   
+            unsolved = manual_input() 
+            # display_board(unsolved)
+            empty = find_blank(unsolved)
+            possible_m = find_moves(empty)
+            test_list = spawn_children(unsolved,possible_m,empty)
+            
+                
+            
+
         elif selection == 3:
             print("Terminating")
             active = False
+
         else:
             print("Improper input, please try again: ")
 
